d58f72ddcc1e1f9130f4345ac14facca8dcdb06f
muxtorjonovazizbek <azizbekmukhtorjonov581@gmail.com>
2024-09-26 21:55:53 +0500

    query params done

--- src/pages/student/index.jsx
+++ src/pages/student/index.jsx
@@ -1,7 +1,7 @@
 import { EditOutlined, ArrowsAltOutlined, DeleteOutlined } from '@ant-design/icons';
 import { Button, Tooltip, Space } from 'antd';
-import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
 import { category } from '@service';
 import { GlobalTable } from '@components';
 import { Category } from '@modal';
@@ -16,6 +16,8 @@ const Index = () => {
     limit: 2,
     page: 1
   })
+  const {search} = useLocation()
+  const navigate = useNavigate()
   const getData = async () => {
    try {
     const res = await category.get([params])
@@ -28,8 +30,20 @@ const Index = () => {
   }
 
   useEffect(()=> {
-    getData(params)
-  },[])
+    getData()
+  },[params])
+
+  useEffect(()=> {
+    const params = new URLSearchParams(search)
+    let page = Number(params.get("page")) || 1
+    let limit = Number(params.get("limit")) || 3
+    setParams((prev)=> ({
+      ...prev,
+      page: page,
+      limit: limit
+    }))
+  }, [search])
+
 
   const editItem = (item) => {
     console.log(item);
@@ -50,7 +64,10 @@ const Index = () => {
       limit: pageSize,
       page: current
     }))
-    console.log(pagination); 
+    const current_params = new URLSearchParams(search)
+    current_params.set('page', `${current}`)
+    current_params.set('limit', `${pageSize}`)
+    navigate(`?${current_params}`)
   }
 
   const handleCancel = ()=> {
@@ -85,7 +102,7 @@ const Index = () => {
     <>
       <h3 className='pl-2 py-2 font-bold fs-4 text-center'>Category</h3>
       <Category open={open} handleCancel={handleCancel} category={update}/>
-      <Button type='primary' className='mb-3 ml-[570px]' onClick={()=> setOpen(true)}>Open modal</Button>
+      <Button type='primary' className='mb-3' onClick={()=> setOpen(true)}>Open modal</Button>
       <GlobalTable
        columns={columns} 
        data={data}
